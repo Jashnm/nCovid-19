1a66a3eafbad00641be2e4d5551867dd1628a0db
Jashn <itsjashn@outlook.com>
2020-04-08 22:46:54 +0530

    add 404 page

--- app.js
+++ app.js
@@ -137,6 +137,9 @@ app.get("/india", (req, res) => {
         res.render('stats-map');
     });
 
+    app.use((req, res, next) => {
+        res.status(404).render('404')
+       })
 
 app.listen(process.env.PORT || 4000, () => {
     console.log("Port started!");
